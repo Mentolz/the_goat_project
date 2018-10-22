7034d6ed53ee0c0db7ac087c9afbe04c334b6e36
Diogo Neto <diogotrofa12@gmail.com>
2018-10-22 14:22:48 +0100

    primeiro commit, os testes estao todos a correr

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,13 @@
+from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+from django.template.loader import render_to_string
 
-class SmokeTest(TestCase):
+from lists.views import home_page
 
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+
+class HomePageTest(TestCase):
+    
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
