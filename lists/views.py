a50ba03fec5e7524dd294515ba74394154aad963
Diogo Neto <diogotrofa12@gmail.com>
2018-10-24 11:59:24 +0100

    Changed the URLs and views

--- lists/views.py
+++ lists/views.py
@@ -5,12 +5,12 @@ from lists.models import Item
 #from IPython import embed; embed()
 
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
     return render(request, 'home.html')
 
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'lists.html', {'items': items})
     
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
